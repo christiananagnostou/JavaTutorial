3a2204aa785ff3f3f06c28e91f07bbad52f61a58
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-11-10 13:39:10 -0800

    refactor printMortgage method and printPaymentSchedule method

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -3,44 +3,54 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
 
+  static Scanner scanner = new Scanner(System.in);
   final static byte MONTHS_IN_YEAR = 12;
   final static byte PERCENT = 100;
 
   public static void main(String[] args) {
     int principal = getPrincipal();
     double monthlyInterestRate = getMonthlyInterestRate();
-    short numMonths = getNumPayments();
+    short numOfMonthlyPayments = getNumberOfMonthlyPayments();
 
-    double mortgage = calculateMortgage(principal, monthlyInterestRate, numMonths);
-    String result = formatCurrency(mortgage);
+    printMortgage(principal, monthlyInterestRate, numOfMonthlyPayments);
+    printPaymentSchedule(principal, monthlyInterestRate, numOfMonthlyPayments);
 
-    System.out.println("Mortgage: " + result);
+    scanner.close();
+  }
 
+  private static void printMortgage(int principal, double monthlyInterestRate, short numOfMonthlyPayments) {
+    double mortgage = calculateMortgage(principal, monthlyInterestRate, numOfMonthlyPayments);
+    String mortgageFormatted = formatCurrency(mortgage);
+    System.out.println("\nMORTGAGE\n--------");
+    System.out.println("Monthly Payments: " + mortgageFormatted);
+  }
+
+  private static void printPaymentSchedule(int principal, double monthlyInterestRate, short numOfMonthlyPayments) {
+    System.out.println("\nPAYMENT SCHEDULE\n----------------");
+    for (short month = 1; month <= numOfMonthlyPayments; month++) {
+      double balance = calculateBalance(principal, monthlyInterestRate, numOfMonthlyPayments, month);
+      String balanceFormatted = formatCurrency(balance);
+      System.out.println(balanceFormatted);
+    }
   }
 
   public static int getPrincipal() {
-    int principal = (int) readNumber("Principal ($1K - $1M): ");
-    boolean isValid = validateNumberRange(1_000, 1_000_000, principal);
-
-    return isValid ? principal : getPrincipal();
+    int principal = (int) readNumber("Principal ($1K - $1M): ", 1_000, 1_000_000);
+    return principal;
   }
 
   public static double getMonthlyInterestRate() {
-    float annualInterest = (float) readNumber("Annual Interest Rate: ");
-    boolean isValid = validateNumberRange(1, 30, annualInterest);
-
-    return isValid ? (annualInterest / MONTHS_IN_YEAR / PERCENT) : getMonthlyInterestRate();
+    float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
+    return (annualInterest / (double) MONTHS_IN_YEAR / (double) PERCENT);
   }
 
-  public static byte getNumPayments() {
-    byte years = (byte) readNumber("Period (Years): ");
-    boolean isValid = validateNumberRange(1, 30, years);
-
-    return isValid ? (byte) (years * MONTHS_IN_YEAR) : getNumPayments();
+  public static byte getNumberOfMonthlyPayments() {
+    byte years = (byte) readNumber("Period (Years): ", 1, 30);
+    return (byte) (years * MONTHS_IN_YEAR);
   }
 
-  public static double calculateMortgage(int principal, double monthlyInterestRate, short numMonths) {
-    double expression = Math.pow(1 + monthlyInterestRate, numMonths);
+  public static double calculateMortgage(int principal, double monthlyInterestRate, short numOfMonthlyPayments) {
+    double expression = Math.pow(1 + monthlyInterestRate, numOfMonthlyPayments);
 
     double top = monthlyInterestRate * expression;
     double bottom = expression - 1;
@@ -48,12 +58,21 @@ public class MortgageCalculator {
     return principal * (top / bottom);
   }
 
+  public static double calculateBalance(int principal, double monthlyInterestRate, short numOfMonthlyPayments,
+      short numPaymentsMade) {
+
+    double numPayments = Math.pow(1 + monthlyInterestRate, numOfMonthlyPayments);
+    double paymentsMade = Math.pow(1 + monthlyInterestRate, numPaymentsMade);
+
+    return principal * ((numPayments - paymentsMade) / (numPayments - 1));
+  }
+
   public static String formatCurrency(double number) {
     return NumberFormat.getCurrencyInstance().format(number);
   }
 
-  public static boolean validateNumberRange(int min, int max, float number) {
-    boolean isWithinRange = (number >= min) && (number <= max);
+  public static boolean validateNumberRange(double value, double min, double max) {
+    boolean isWithinRange = (value >= min) && (value <= max);
 
     if (!isWithinRange)
       System.out.println(String.format("Invalid input. Please enter a value between %s and %s", min, max));
@@ -61,13 +80,14 @@ public class MortgageCalculator {
     return isWithinRange;
   }
 
-  public static double readNumber(String prompt) {
-    System.out.println(prompt);
+  public static double readNumber(String prompt, double min, double max) {
+    System.out.print(prompt);
 
-    Scanner scanner = new Scanner(System.in);
     double value = scanner.nextFloat();
-    scanner.close();
 
-    return value;
+    boolean isValid = validateNumberRange(value, min, max);
+
+    return isValid ? value : readNumber(prompt, min, max);
   }
+
 }
